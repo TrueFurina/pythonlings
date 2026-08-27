8cb427dc1e75b8f722227211516b8f457ce2c13b
Dream <2468001320@qq.com>
2026-08-27 11:18:28 +0800

    curriculum: add assertion message to checks/oop_advanced/oop_advanced12.py

--- checks/oop_advanced/oop_advanced12.py
+++ checks/oop_advanced/oop_advanced12.py
@@ -1,4 +1,4 @@
 result = Vector(1, 2) + Vector(3, 4)
-assert isinstance(result, Vector)
-assert (result.x, result.y) == (4, 6)
+assert isinstance(result, Vector), "Adding two Vector instances should return a Vector"
+assert (result.x, result.y) == (4, 6), "Vector(1,2) + Vector(3,4) should be (4, 6)"
 print("oop_advanced12 ok")
