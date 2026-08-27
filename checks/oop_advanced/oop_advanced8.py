59e7e96905c5c6ce412749809a71f89b6d9fcd41
Dream <2468001320@qq.com>
2026-08-27 11:18:15 +0800

    curriculum: add assertion message to checks/oop_advanced/oop_advanced8.py

--- checks/oop_advanced/oop_advanced8.py
+++ checks/oop_advanced/oop_advanced8.py
@@ -1,3 +1,3 @@
 coord = Coordinate.from_text("3,4")
-assert (coord.x, coord.y) == (3, 4)
+assert (coord.x, coord.y) == (3, 4), "Coordinate.from_text('3,4') should produce x=3 and y=4"
 print("oop_advanced8 ok")
